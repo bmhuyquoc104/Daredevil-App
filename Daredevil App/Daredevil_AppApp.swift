5ade4044fc281717bdd9fd2e5a3b582b35285d83
Huy ne <bmhuyquoc104@gmail.com>
2022-07-26 13:57:58 +0700

    1. Set up project

---
+++ Daredevil App/Daredevil_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daredevil_AppApp.swift
+//  Daredevil App
+//
+//  Created by Võ Quốc Huy on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Daredevil_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ Daredevil App/Views/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Daredevil App
+//
+//  Created by Võ Quốc Huy on 26/07/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
